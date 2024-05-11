c5b4b17cd43ae14145aa19db7d69a43a947e75c5
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2024-05-12 01:24:40 +0530

    commit

--- assets/js/firebaseconfig.js
+++ assets/js/firebaseconfig.js
@@ -1,5 +1,4 @@
-
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBncdnP33SF5gVSACcn2DI7DMZ-eIF3hj4",
     authDomain: "cashsnap-9a185.firebaseapp.com",
@@ -9,6 +8,4 @@ const firebaseConfig = {
     messagingSenderId: "1046320871049",
     appId: "1:1046320871049:web:600b3dee8af7b3c4763858",
     measurementId: "G-5M366WH052"
-};
-
-
+  };
