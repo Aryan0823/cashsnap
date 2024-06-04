eb7ba30372c3239eae6a2fc1c60f3339535be1dd
Aryan0823 <90816415+Aryan0823@users.noreply.github.com>
2024-06-04 18:10:59 +0530

    commit

--- assets/js/chart-custom.js
+++ assets/js/chart-custom.js
@@ -5216,31 +5216,9 @@ if (jQuery("#editor").length) {
     chart.scrollbarX = new am4core.Scrollbar();
     
     // Add data
-    chart.data = [{
-      "country": "Jan",
-      "visits": 3025
-    }, {
-      "country": "Feb",
-      "visits": 1882
-    }, {
-      "country": "Mar",
-      "visits": 1809
-    }, {
-      "country": "Apr",
-      "visits": 1322
-    }, {
-      "country": "May",
-      "visits": 1122
-    }, {
-      "country": "Jun",
-      "visits": 1114
-    }, {
-      "country": "Jul",
-      "visits": 984
-    }, {
-      "country": "Aug",
-      "visits": 711
-    }];
+    chart.data = [
+
+    ];
     
     prepareParetoData();
     
